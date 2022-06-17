0d3cb7c02488b22d915b616a5ff83a8bf3d642bb
haris.munandar <haris.munandar@bps.go.id>
2022-06-18 00:17:37 +0800

    dkjjkds

---
+++ app.R
@@ -0,0 +1,71 @@
+library(shiny)
+#library(shiny.semantic)
+#library(semantic.dashboard)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(tidyr)
+library(stringr)
+library(plotly)
+library(gsheet)
+
+  library(shiny)
+  library(shinyMobile)
+
+    ui = f7Page(
+      title = "Split layout",
+      f7SplitLayout(
+        sidebar = f7Panel(
+          id = "sidebar",
+          title = "Sidebar",
+          side = "left",
+          theme = "dark",
+          selectInput("kec","Pilih Kecamatan",choices=c("Pangkep","Liukang Tangaya","Liukang Kalmas","Liukang Tupabbiring","Liukang Tupabbiring Utara","Pangkajene","Minasatene","Balocci","Tondong Tallasa","Bungoro","Labakkang","Ma'rang","Segeri","Mandalle"),selected = "Pangkep")
+          
+          
+        ),
+        navbar = f7Navbar(
+          title = "Split Layout",
+          hairline = FALSE,
+          shadow = TRUE
+        ),
+        # main content
+        f7Shadow(
+          intensity = 10,
+          hover = TRUE,
+          sliderInput("obs", "Number of observations", 0, 1000, 500),
+          
+          plotOutput("Plot")
+        )
+      )
+    )
+    server = function(input, output) {
+      showNotification("Mohon, Tunggu",type="message")
+      dataku=reactive({
+        url="https://docs.google.com/spreadsheets/d/1t0BpPpeMU2ts0EmQgL2Hf5yOa3ldjIz9LuiVRJLzi9w/edit?usp=sharing"
+        gsheet2tbl(url)
+      })
+      edcod=reactive({
+        url="https://docs.google.com/spreadsheets/d/1fUjRHX4HN2n0skF-X3UsfxCkVgR1_SyaIBVkpxK0X-U/edit?usp=sharing"
+        gsheet2tbl(url)
+      })
+      data2=reactive({
+        dataku()%>% group_by(Kecamatan) %>%
+          summarise(total_target_BS=sum(Target.BS,na.rm = T),total_BS_Selesai=sum(BS_selesai,na.rm = T),total_target_RT=sum(Target.RT,na.rm = T),total_RT_Selesai=sum(RT_selesai,na.rm = T))%>%
+          mutate( persen_BS=total_BS_Selesai/total_target_BS)%>%
+          mutate( persen_RT=total_RT_Selesai/total_target_RT)%>%
+          mutate(color=ifelse(persen_RT<.5,1,ifelse(persen_RT<0.8,2,3)))
+      })
+      
+    
+      
+      output$Plot <- renderPlot({
+        dist <- rnorm(input$obs)
+        hist(dist)
+      })
+    }
+  
+
+    shinyApp(ui,server)
